3087a3f3fada2221bbbe9301bcc6f2f718b1103b
MaridiM <maridim92@gmail.com>
2025-04-16 15:44:47 +0300

    add paths

--- apps/patients/next.config.ts
+++ apps/patients/next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
+import path from 'path';
 
 const withNextIntl = createNextIntlPlugin('./src/shared/libs/i18n/request.ts')
 
 const nextConfig: NextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+      webpack: (config) => {
+        config.resolve.alias["@ui"] = path.resolve(__dirname, "../../packages/ui/src");
+        config.resolve.alias["@config"] = path.resolve(__dirname, "../../packages/config/src");
+        return config;
+      },
 }
 
 export default withNextIntl(nextConfig)
